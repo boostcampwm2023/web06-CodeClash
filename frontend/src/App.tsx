e9551c76535b57194f38fc944cf2fd72d033f773
leegurts <rmstjd333@gmail.com>
2023-11-07 11:43:46 +0900

    init: 프론트엔드 프로젝트 기초 설정
    - React
    - CRA
    - Zustand
    - React Query
    - Tailwindcss

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Provider from "./components/provider";
+
+const App = () => {
+    return (
+        <Provider>
+            <div className="App">우리의 꿈</div>
+        </Provider>
+    );
+};
+
+export default App;
