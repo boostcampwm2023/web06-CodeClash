1feed0e078fea6415fcfb0af643ced3618d58926
h9661 <guscksdn123@gmail.com>
2023-11-13 17:27:25 +0900

    [Feat] 채점 컨트롤러 추가

--- src/scores/scores.controller.ts
+++ src/scores/scores.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { ScoresService } from './scores.service';
 
-@Controller('scores')
+interface IUserSubmission {
+  code: string;
+  language: string;
+  problemNumber: number;
+}
+
+@Controller('v2/scores')
 export class ScoresController {
   constructor(private readonly scoresService: ScoresService) {}
+
+  @Post('/grade')
+  async grade(@Body() submission: IUserSubmission) {
+    const { code, language, problemNumber } = submission;
+
+    return await this.scoresService.grade(code, language, problemNumber);
+  }
 }
--- src/scores/scores.service.ts
+++ src/scores/scores.service.ts
@@ -1,3 +1,21 @@
 import { Injectable } from '@nestjs/common';
+
+const SCORING_SERVER = 'http://106.10.57.235:3000';
 @Injectable()
-export class ScoresService {}
+export class ScoresService {
+  async grade(code: string, language: string, problemNumber: number) {
+    const response = await fetch(`${SCORING_SERVER}/v2/scoring`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        code,
+        testcase: {
+          parameters: [10, 20],
+          answer: 200,
+        },
+      }),
+    });
+
+    return response.json();
+  }
+}
