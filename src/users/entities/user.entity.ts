94c6c7ef18b0047f852070a4f2be50e430fa4b30
h9661 <guscksdn123@gmail.com>
2023-11-14 13:23:33 +0900

    [Feat] Add class-validator to user entity for input validation

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,4 +1,10 @@
 import {
+  IsEmail,
+  IsString,
+  Length,
+  ValidationArguments,
+} from 'class-validator';
+import {
   Column,
   CreateDateColumn,
   Entity,
@@ -23,12 +29,45 @@ export class UserTable {
   updatedAt: Date;
 
   @Column({ length: 500, unique: true })
+  @IsString({
+    message: (args: ValidationArguments) => {
+      return `${args.property} must be a string`;
+    },
+  })
+  @Length(6, 20, {
+    message: (args: ValidationArguments) => {
+      return `${args.property} must be between 6 and 20 characters`;
+    },
+  })
   name: string;
 
   @Column({ length: 500 })
+  @IsString({
+    message: (args: ValidationArguments) => {
+      return `${args.property} must be a string`;
+    },
+  })
+  @IsEmail(
+    {},
+    {
+      message: (args: ValidationArguments) => {
+        return `${args.property} must be a valid email`;
+      },
+    },
+  )
   email: string;
 
   @Column({ length: 500 })
+  @IsString({
+    message: (args: ValidationArguments) => {
+      return `${args.property} must be a string`;
+    },
+  })
+  @Length(6, 20, {
+    message: (args: ValidationArguments) => {
+      return `${args.property} must be between 6 and 20 characters`;
+    },
+  })
   password: string;
 
   @Column({ type: 'enum', enum: Role, default: Role.USER })
