f45ca7658d1a39f08b477a63c637f301a960dec5
h9661 <guscksdn123@gmail.com>
2023-11-14 20:04:04 +0900

    [Feat] Add email registration endpoint and RegisterUserDto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { RegisterUserDto } from './dto/register-user.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('register/email')
+  registerByEmail(@Body() registerByEmailDto: RegisterUserDto) {
+    return this.authService.registerWithEmail(registerByEmailDto);
+  }
 }
