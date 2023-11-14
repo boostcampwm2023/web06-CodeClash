127a71cee495bcf81ec6d4267ef40d7d36fe731d
h9661 <guscksdn123@gmail.com>
2023-11-14 19:42:35 +0900

    [Feat] Add user creation functionality.

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserTable } from './entities/user.entity';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
+  imports: [TypeOrmModule.forFeature([UserTable])],
+  exports: [UsersService],
 })
 export class UsersModule {}
