888756884ddc154be9831fb9c3290828abd8f670
leegurts <rmstjd333@gmail.com>
2023-11-14 18:24:02 +0900

    [FE] Feat: create LobyHeader

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import Button from "./components/common/Button";
 import GamePlayPage from "./pages/GamePlayPage";
+import LobyPage from "./pages/LobyPage";
 
 const App: React.FC = () => {
   return (
     <div className="bg-defaultPattern h-[100vh] w-[100vw]">
-      <GamePlayPage />
+      <LobyPage />
     </div>
   );
 };
---
+++ frontend/src/pages/LobyPage.tsx
@@ -0,0 +1,11 @@
+import LobyHeader from "../components/loby/LobyHeader";
+
+const LobyPage: React.FC = () => {
+  return (
+    <>
+      <LobyHeader />
+    </>
+  );
+};
+
+export default LobyPage;
