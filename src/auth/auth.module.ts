e91856b035da7c3f0f62823af462e21006fe85ab
h9661 <guscksdn123@gmail.com>
2023-11-14 19:45:37 +0900

    [Chore] Add UsersModule to AuthModule imports and inject UsersService into AuthService constructor.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [JwtModule.register({})],
+  imports: [JwtModule.register({}), UsersModule],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly usersService: UsersService,
+  ) {}
 }
