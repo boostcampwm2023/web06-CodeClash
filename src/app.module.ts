afa9be5472d1db88a59afce3fe40fa9230b2f93d
h9661 <guscksdn123@gmail.com>
2023-11-14 12:54:33 +0900

    [Chore] Add TypeORM module for database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ScoresModule } from './scores/scores.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST_DEV,
+      port: parseInt(process.env.DB_PORT_DEV),
+      username: process.env.DB_USERNAME_DEV,
+      password: process.env.DB_PASSWORD_DEV,
+      database: process.env.DB_DATABASE_DEV,
+      synchronize: true,
+      entities: [],
     }),
     ScoresModule,
   ],
