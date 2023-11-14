ab142c10c5a0f014afae696d821ffbb299804f02
h9661 <guscksdn123@gmail.com>
2023-11-14 20:21:52 +0900

    [Feat] Add email login endpoint with BasicTokenGuard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterUserDto } from './dto/register-user.dto';
+import { BasicTokenGuard } from './guard/basic-token.guard';
+import { User } from 'src/users/decorators/user.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('login/email')
+  @UseGuards(BasicTokenGuard)
+  loginByEmail(@User() user) {
+    const { email, password } = user;
+    return this.authService.loginWithEmail({ email, password });
+  }
 
   @Post('register/email')
   registerByEmail(@Body() registerByEmailDto: RegisterUserDto) {
