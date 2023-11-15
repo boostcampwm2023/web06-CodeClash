dc1221669deaaf4431aa5a1c62deaba51213b83e
h9661 <guscksdn123@gmail.com>
2023-11-15 11:34:56 +0900

    [Refactor] dto, authentication을 사용하도록 변경 Add authentication and refactor submission handling in ScoresController and ScoresService

--- src/scores/scores.service.ts
+++ src/scores/scores.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ScoreSubmissionDto } from './dto/score-submission.dto';
+import { UserTable } from 'src/users/entities/user.entity';
 
 const SCORING_SERVER = 'http://10.41.177.25:3000';
 @Injectable()
 export class ScoresService {
-  async grade(code: string, language: string, problemNumber: number) {
+  async grade(submission: ScoreSubmissionDto, user: UserTable) {
+    const { code } = submission;
+    const { email } = user;
+
     const response = await fetch(`${SCORING_SERVER}/v2/scoring`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
