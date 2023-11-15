c1bad28dd5204236a41b7e0948a6dcab514a00f5
h9661 <guscksdn123@gmail.com>
2023-11-15 11:14:33 +0900

    [Feat] AccessTokenGuard, RefreshTokenGuard 구현 Add BearerTokenGuard, AccessTokenGuard, and RefreshTokenGuard to handle token authentication

---
+++ src/auth/guard/bearer-token.guard.ts
@@ -0,0 +1,63 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+class BearerTokenGuard implements CanActivate {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly usersService: UsersService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const req = context.switchToHttp().getRequest();
+    const rawToken = req.headers.authorization;
+
+    if (!rawToken) {
+      throw new UnauthorizedException('no token provided');
+    }
+
+    const token = this.authService.extractTokenFromHeader(rawToken, true);
+    const decode = this.authService.verifyToken(token);
+    const user = await this.usersService.getUserByEmail(decode.email);
+
+    req.token = token;
+    req.tokenType = decode.type;
+    req.user = user;
+
+    return true;
+  }
+}
+
+@Injectable()
+export class AccessTokenGuard extends BearerTokenGuard {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    await super.canActivate(context);
+    const req = context.switchToHttp().getRequest();
+
+    if (req.tokenType !== 'access') {
+      throw new UnauthorizedException('this is not access token');
+    }
+
+    return true;
+  }
+}
+
+@Injectable()
+export class RefreshTokenGuard extends BearerTokenGuard {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    await super.canActivate(context);
+    const req = context.switchToHttp().getRequest();
+
+    if (req.tokenType !== 'refresh') {
+      throw new UnauthorizedException('this is not refresh token');
+    }
+
+    return true;
+  }
+}
