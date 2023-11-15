d8227ad790900e3effc60e794fd3155c22600efa
ruokic <gusqo327@naver.com>
2023-11-15 17:26:51 +0900

    [FE] Feat: 결과 페이지 작성
    - 화면 좌측에 유저 점수와 다시하기/나가기 버튼
    - 화면 우측에 문제 별 코드 확인

--- frontend/src/components/gameplay/GameDefaultBox.tsx
+++ frontend/src/components/gameplay/GameDefaultBox.tsx
@@ -6,7 +6,7 @@ interface GameDefaultBoxProps {
 const GameDefaultBox: React.FC<GameDefaultBoxProps> = ({ children, className = "" }) => {
   return (
     <div className={"flex p-3 h-full w-full rounded-3xl border-8 border-white bg-skyblue " + className}>
-      <div className="p-2 h-full w-full rounded-xl bg-lightskyblue">{children}</div>
+      <div className="p-2 h-full w-full rounded-xl bg-lightskyblue overflow-scroll">{children}</div>
     </div>
   );
 };
