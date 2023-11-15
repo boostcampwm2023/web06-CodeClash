ec7535f1659a0d1996ba07e8fffe0c1ddc76f8cd
h9661 <guscksdn123@gmail.com>
2023-11-15 21:09:33 +0900

    [Test] 테스트 실패 해결 Add UsersService and UsersMockRepository to providers in auth and scores controller specs

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -2,6 +2,10 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { UserTable } from 'src/users/entities/user.entity';
+import { UsersMockRepository } from 'src/users/mocks/repository.mock';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -9,7 +13,15 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService, JwtService],
+      providers: [
+        AuthService,
+        JwtService,
+        UsersService,
+        {
+          provide: getRepositoryToken(UserTable),
+          useClass: UsersMockRepository,
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,13 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { UserTable } from 'src/users/entities/user.entity';
+import { UsersMockRepository } from 'src/users/mocks/repository.mock';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService, JwtService],
+      providers: [
+        AuthService,
+        JwtService,
+        UsersService,
+        {
+          provide: getRepositoryToken(UserTable),
+          useClass: UsersMockRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
