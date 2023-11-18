e179a4d50a762ef7be86c489e73e72a4ba33ffe7
h9661 <guscksdn123@gmail.com>
2023-11-18 22:05:50 +0900

    Add mock repository for testcases and users

--- src/testcases/testcases.service.spec.ts
+++ src/testcases/testcases.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TestcasesService } from './testcases.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { TestcasesMockRepository } from './mocks/repository.mock';
+import { TestcaseTable } from './entities/testcase.entity';
 
 describe('TestcasesService', () => {
   let service: TestcasesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TestcasesService],
+      providers: [
+        TestcasesService,
+        {
+          provide: getRepositoryToken(TestcaseTable),
+          useClass: TestcasesMockRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<TestcasesService>(TestcasesService);
