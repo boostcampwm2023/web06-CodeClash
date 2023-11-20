26815fd35c1783b810d1619e6e641df155e5fbf7
h9661 <guscksdn123@gmail.com>
2023-11-20 11:51:37 +0900

    Add mock repository for Submissions entity

--- src/submissions/submissions.controller.spec.ts
+++ src/submissions/submissions.controller.spec.ts
@@ -1,6 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SubmissionsController } from './submissions.controller';
 import { SubmissionsService } from './submissions.service';
+import { AuthModule } from 'src/auth/auth.module';
+import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { SubmissionTable } from './entities/submission.entity';
+import { SubmissionsMockRepository } from './mocks/repository.mock';
 
 describe('SubmissionsController', () => {
   let controller: SubmissionsController;
@@ -8,7 +14,16 @@ describe('SubmissionsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SubmissionsController],
-      providers: [SubmissionsService],
+      providers: [
+        SubmissionsService,
+        AuthModule,
+        UsersModule,
+        JwtModule,
+        {
+          provide: getRepositoryToken(SubmissionTable),
+          useClass: SubmissionsMockRepository,
+        },
+      ],
     }).compile();
 
     controller = module.get<SubmissionsController>(SubmissionsController);
--- src/submissions/submissions.service.spec.ts
+++ src/submissions/submissions.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SubmissionsService } from './submissions.service';
+import { SubmissionTable } from './entities/submission.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { SubmissionsMockRepository } from './mocks/repository.mock';
 
 describe('SubmissionsService', () => {
   let service: SubmissionsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SubmissionsService],
+      providers: [
+        SubmissionsService,
+        {
+          provide: getRepositoryToken(SubmissionTable),
+          useClass: SubmissionsMockRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<SubmissionsService>(SubmissionsService);
