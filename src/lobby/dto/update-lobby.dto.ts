41ce25616c358d1711062aa7c8ccc413af429c0b
h9661 <guscksdn123@gmail.com>
2023-11-20 18:31:16 +0900

    Delete lobby-related files
    Co-authored-by: movingsummer <movingsummer@users.noreply.github.com>

--- src/lobby/dto/update-lobby.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateLobbyDto } from './create-lobby.dto';
-
-export class UpdateLobbyDto extends PartialType(CreateLobbyDto) {
-  id: number;
-}
--- src/lobby/lobby.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { LobbyService } from './lobby.service';
-import { LobbyGateway } from './lobby.gateway';
-
-@Module({
-  providers: [LobbyGateway, LobbyService],
-})
-export class LobbyModule {}
