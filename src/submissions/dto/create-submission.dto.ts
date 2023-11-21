a1866bc3689a7d1b2937488cfdc639c2d22b2d6b
h9661 <guscksdn123@gmail.com>
2023-11-21 15:06:09 +0900

    Add fields to CreateSubmissionDto and remove unused fields from SubmissionTable
    Co-authored-by: movingsummer <movingsummer@users.noreply.github.com>

--- src/submissions/dto/create-submission.dto.ts
+++ src/submissions/dto/create-submission.dto.ts
@@ -1,1 +1,11 @@
-export class CreateSubmissionDto {}
+import { PickType } from '@nestjs/mapped-types';
+import { SubmissionTable } from '../entities/submission.entity';
+
+export class CreateSubmissionDto extends PickType(SubmissionTable, [
+  'code',
+  'status',
+  'language',
+]) {
+  userId: number;
+  problemId: number;
+}
--- src/submissions/submissions.service.ts
+++ src/submissions/submissions.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { SubmissionTable } from './entities/submission.entity';
 import { Repository } from 'typeorm';
+import { CreateSubmissionDto } from './dto/create-submission.dto';
 
 @Injectable()
 export class SubmissionsService {
@@ -18,5 +19,10 @@ export class SubmissionsService {
     return await this.submissionsRepository.findOne({
       where: { user: { name: userName } },
     });
+  }
+
+  async createSubmission(createSubmissionDto: CreateSubmissionDto) {
+    const submission = this.submissionsRepository.create(createSubmissionDto);
+    return await this.submissionsRepository.save(submission);
   }
 }
