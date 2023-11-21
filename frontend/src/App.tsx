669e50be8988030974ddf5c4d219a7cf304af77f
leegurts <rmstjd333@gmail.com>
2023-11-21 10:48:22 +0900

    [FE] feature: 메인 주소 접속시 리다이렉션

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,16 +4,15 @@ import ReadyPage from "./pages/ReadyPage";
 import GamePlayPage from "./pages/GamePlayPage";
 import ResultPage from "./pages/ResultPage";
 import LoginPage from "./pages/LoginPage";
-import Provider from "./components/Provider";
 import { useLoginStore } from "./store/useLogin";
 import ProtectRoute from "./components/protectroute/ProtectRoute";
 
 const App: React.FC = () => {
-  const { isLogin } = useLoginStore();
   return (
     <div className="bg-defaultPattern h-[100vh] w-[100vw] min-w-[1180px]">
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<LoginPage />} />
           <Route element={<ProtectRoute to="/login" />}>
             <Route path="/lobby" element={<LobbyPage />} />
