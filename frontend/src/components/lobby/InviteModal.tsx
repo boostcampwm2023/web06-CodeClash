404d9af74209303d186e60ad98c97d8097f63b1d
ruokic <gusqo327@naver.com>
2023-11-22 14:28:58 +0900

    [FE] fix: 초대 리스트 map에 key 추가

--- frontend/src/components/lobby/InviteModal.tsx
+++ frontend/src/components/lobby/InviteModal.tsx
@@ -11,7 +11,10 @@ const InviteModal: React.FC<InviteModalProps> = ({ closeModal, inviteList }) => 
   return (
     <Modal title="초대 리스트" closeModal={closeModal} className="h-[600px] px-2 flex flex-col overflow-scroll">
       {inviteList.map(({ roomName, host }) => (
-        <div className="flex justify-between skew-x-right rounded-lg px-4 text-white text-sm hover:bg-lightskyblue hover:text-black">
+        <div
+          className="flex justify-between skew-x-right rounded-lg px-4 text-white text-sm hover:bg-lightskyblue hover:text-black"
+          key={roomName + host}
+        >
           <div className="grid w-[550px] p-2 skew-x-left">
             <div className="truncate">{roomName}</div>
             <div className="justify-self-end">{host}</div>
