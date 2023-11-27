8bc6e224d8fe284a664fa9b5b961f7e013c6d94e
h9661 <guscksdn123@gmail.com>
2023-11-27 12:06:55 +0900

    Delete unnecessary files and configurations

--- frontend/.prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    semi: true,
-    useTabs: false,
-    tabWidth: 2,
-    trailingComma: "all",
-    printWidth: 120,
-    arrowParens: "avoid",
-    disableLanguages: ["html", "ejs"],
-};
--- frontend/src/components/room/ReadyMark.tsx
+++
@@ -1,9 +0,0 @@
-const ReadyMark: React.FC = () => {
-  return (
-    <div className="z-10 absolute rotate-[-45deg] top-[40%] left-[-72px] w-48 bg-pink p-2 border-8 rounded-xl border-yellow">
-      READY
-    </div>
-  );
-};
-
-export default ReadyMark;
