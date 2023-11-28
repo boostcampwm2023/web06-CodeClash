f100567a88b1450db2d6e4fd13b7f1ba50deaaa2
movingsummer <movingsummer0x16@gmail.com>
2023-11-28 15:50:31 +0900

    fix: socket.data가 undefined 경우 예외처리
    handleDisconnection에서 exitRoom을 실행할 때 socket.rooms와 socket.data가 초기화되므로 예외 처리를 했다.

--- api-server/src/rooms/rooms.service.ts
+++ api-server/src/rooms/rooms.service.ts
@@ -60,8 +60,10 @@ export class RoomsService {
   }
 
   exitRoom(client: Socket, roomId: string) {
-    client.leave(roomId);
-    client.data.user.ready = false;
+    if (client.rooms) {
+      client.leave(roomId);
+      client.data.user.ready = false;
+    }
     this.deleteUserFromList(client, roomId);
 
     if (roomId !== 'lobby' && this.roomList[roomId].userList.length === 0) {
