ca199f3628bc7832e5296540492bc229132093f1
movingsummer <movingsummer0x16@gmail.com>
2023-11-28 15:45:18 +0900

    chore: 필요없는 의존성 제거, nestFactory 누락 추가
    누락된 모듈을 추가하고 필요없는 의존성을 제거했다.

--- api-server/src/main.ts
+++ api-server/src/main.ts
@@ -1,5 +1,5 @@
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { LogInterceptor } from './common/interceptors/log.interceptor';
 import { SocketIoAdapter } from './adapters/socket.io.adapter';
 import * as cookesParser from 'cookie-parser';
 import { logger } from './utils/logger/winston.config';
