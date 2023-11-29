1afe4adedc1c743bf3680bbb86ce8916440992c9
leegurts <rmstjd333@gmail.com>
2023-11-29 17:51:35 +0900

    [FE] feat: game 아이템 아이콘 추가

---
+++ frontend/src/assets/svg/DelaySvg.tsx
@@ -0,0 +1,19 @@
+interface DelaySvgProps {
+  className?: string;
+}
+
+const DelaySvg: React.FC<DelaySvgProps> = ({ className }) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" className={className} viewBox="0 0 24 24" fill="none">
+      <path
+        d="M15.24 2H8.76001C5.00001 2 4.71001 5.38 6.74001 7.22L17.26 16.78C19.29 18.62 19 22 15.24 22H8.76001C5.00001 22 4.71001 18.62 6.74001 16.78L17.26 7.22C19.29 5.38 19 2 15.24 2Z"
+        stroke="#292D32"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default DelaySvg;
