c00fcef148a2b0dbf7eee901b7fee759262b1a2b
leegurts <rmstjd333@gmail.com>
2023-11-30 16:48:32 +0900

    [FE] Fix: 준비 아이콘 위치 버그 수정

--- frontend/src/components/gameplay/problemType.ts
+++ frontend/src/components/gameplay/problemType.ts
@@ -1,15 +1,14 @@
 export interface ProblemType {
-  createdAt: string;
-  description: string;
   id: number;
+  title: string;
+  description: string;
+  timeLimit: number;
   memoryLimit: number;
   sampleCode: string;
-  testcases: {
-    id: number;
-    input: string;
-    output: string;
-  }[];
-  title: string;
-  timeLimit: number;
-  updatedAt: string;
+  testcases: Testcase[];
+}
+
+interface Testcase {
+  input: string;
+  output: string;
 }
