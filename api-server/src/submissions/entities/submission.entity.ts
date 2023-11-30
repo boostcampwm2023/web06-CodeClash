adbbffd95249c9477550c2e790934a74ff78874e
h9661 <guscksdn123@gmail.com>
2023-11-30 15:52:40 +0900

    [Feat] 코드 해쉬 컬럼 추가 Add codeHash column to SubmissionTable

--- api-server/src/submissions/entities/submission.entity.ts
+++ api-server/src/submissions/entities/submission.entity.ts
@@ -1,7 +1,7 @@
 import { BaseTable } from 'src/common/entities/base-table.entity';
 import { ProblemTable } from 'src/problems/entities/problem.entity';
 import { UserTable } from 'src/users/entities/user.entity';
-import { Column, Entity, ManyToOne } from 'typeorm';
+import { Column, Entity, Index, ManyToOne } from 'typeorm';
 
 export enum SubmissionStatus {
   Accepted = 'Accepted',
@@ -23,6 +23,10 @@ export class SubmissionTable extends BaseTable {
 
   @Column({ type: 'enum', enum: SubmissionStatus })
   status: SubmissionStatus;
+
+  @Column({ type: 'text' })
+  @Index()
+  codeHash: string;
 
   @ManyToOne(() => UserTable, (user) => user.submissions)
   user: UserTable;
