c8e97b053e6ec9e8a5bc7c6fe86ed4d1ab76068f
h9661 <guscksdn123@gmail.com>
2023-11-30 15:15:00 +0900

    [Feat] ValidationPipe 전역 설정

--- api-server/src/main.ts
+++ api-server/src/main.ts
@@ -3,11 +3,22 @@ import { AppModule } from './app.module';
 import { SocketIoAdapter } from './adapters/socket.io.adapter';
 import * as cookesParser from 'cookie-parser';
 import { logger } from './utils/logger/winston.config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger,
   });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   app.useWebSocketAdapter(new SocketIoAdapter(app));
   app.enableCors({
     origin: true,
