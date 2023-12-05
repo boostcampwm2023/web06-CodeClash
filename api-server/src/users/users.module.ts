5f56386f42020dbd52376f08d2bf90fdbc435035
h9661 <guscksdn123@gmail.com>
2023-12-05 15:35:31 +0900

    [Chore] SubmissionsModule import

--- api-server/src/users/users.module.ts
+++ api-server/src/users/users.module.ts
@@ -3,11 +3,12 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserTable } from './entities/user.entity';
+import { SubmissionsModule } from 'src/submissions/submissions.module';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
-  imports: [TypeOrmModule.forFeature([UserTable])],
+  imports: [TypeOrmModule.forFeature([UserTable]), SubmissionsModule],
   exports: [UsersService],
 })
 export class UsersModule {}
