74a696f4dcfdd046c79b20f7a9398aadf3316988
h9661 <guscksdn123@gmail.com>
2023-12-19 13:02:14 +0900

    [Chore] 최대 채점 갯수 변경

--- scoring-server/codeRunner.js
+++ scoring-server/codeRunner.js
@@ -5,7 +5,7 @@ const queue = require("express-queue");
 const app = express();
 
 app.use(express.json());
-app.use(queue({ activeLimit: 4, queuedLimit: -1 }));
+app.use(queue({ activeLimit: 5, queuedLimit: -1 }));
 
 app.post("/v2/scoring", (req, res) => {
   const { code, testcase, timeLimit, memoryLimit, isExample } = req.body;
