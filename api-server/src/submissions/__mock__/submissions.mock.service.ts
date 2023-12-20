b51d21e26d9cb2abe9054ddc24aaf04312d205f1
h9661 <guscksdn123@gmail.com>
2023-12-20 21:15:42 +0900

    Delete mock service files

--- api-server/src/submissions/__mock__/submissions.mock.service.ts
+++
@@ -1,8 +0,0 @@
-export const SubmissionsMockService = jest.fn().mockReturnValue({
-  paginateSubmissionsByUserId: jest.fn(() => {
-    return [];
-  }),
-  getCountOfSubmissionsByUserId: jest.fn(() => {
-    return 0;
-  }),
-});
