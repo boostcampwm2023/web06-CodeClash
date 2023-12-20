7c9e8bba196a193ba6568b5106a3ff5f1d3a9e19
h9661 <guscksdn123@gmail.com>
2023-12-20 17:41:07 +0900

    [Feat] Unit test, e2e test code 작성

--- api-server/src/users/users.controller.spec.ts
+++ api-server/src/users/users.controller.spec.ts
@@ -1,37 +1,145 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { UserTable } from './entities/user.entity';
-import { getRepositoryToken } from '@nestjs/typeorm';
-import { UsersMockRepository } from './mocks/repository.mock';
 import { SubmissionsService } from 'src/submissions/submissions.service';
-import { SubmissionsMockRepository } from 'src/submissions/mocks/repository.mock';
-import { SubmissionTable } from 'src/submissions/entities/submission.entity';
+import { INestApplication, NotFoundException } from '@nestjs/common';
+import * as request from 'supertest';
 
-describe('UsersController', () => {
+describe('UsersController unit test', () => {
   let controller: UsersController;
+  let app: INestApplication;
+  let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
       providers: [
-        UsersService,
-        SubmissionsService,
         {
-          provide: getRepositoryToken(UserTable),
-          useClass: UsersMockRepository,
+          provide: UsersService,
+          useValue: {
+            getUserByName: jest.fn((name) => {
+              if (name == 'test') {
+                return {
+                  name: 'test',
+                  email: 'test@test.test',
+                  acceptCount: 0,
+                  failCount: 0,
+                  winCount: 0,
+                  totalCount: 0,
+                };
+              } else {
+                throw new NotFoundException('User not found');
+              }
+            }),
+          },
         },
         {
-          provide: getRepositoryToken(SubmissionTable),
-          useClass: SubmissionsMockRepository,
+          provide: SubmissionsService,
+          useValue: {
+            paginateSubmissionsByUserId: jest.fn(() => {
+              return [];
+            }),
+            getCountOfSubmissionsByUserId: jest.fn(() => {
+              return 0;
+            }),
+          },
         },
       ],
     }).compile();
 
+    app = module.createNestApplication();
+    await app.init();
     controller = module.get<UsersController>(UsersController);
+    service = await module.resolve<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return user stats', async () => {
+    const result = {
+      name: 'test',
+      email: 'test@test.test',
+      acceptCount: 0,
+      failCount: 0,
+      winCount: 0,
+      totalCount: 0,
+    };
+
+    expect(await controller.getUserStats('test')).toEqual(result);
+  });
+
+  it('if user not found, should throw NotFoundException', async () => {
+    await expect(controller.getUserStats('notfound')).rejects.toThrow(
+      'User not found',
+    );
+  });
+
+  it('test 200 /api/users/stats/:userName', async () => {
+    return request(app.getHttpServer())
+      .get('/api/users/stats/test')
+      .expect(200)
+      .expect({
+        ...service.getUserByName('test'),
+      });
+  });
+
+  it('test 404 /api/users/stats/:userName', async () => {
+    return request(app.getHttpServer())
+      .get('/api/users/stats/notfound')
+      .expect(404)
+      .expect({
+        statusCode: 404,
+        message: 'User not found',
+        error: 'Not Found',
+      });
+  });
+
+  it('should return user info', async () => {
+    const result = {
+      name: 'test',
+      email: 'test@test.test',
+      acceptCount: 0,
+      failCount: 0,
+      winCount: 0,
+      totalCount: 0,
+      submissions: [],
+      pageEnd: 0,
+    };
+
+    expect(await controller.getUserInfo('test', '0', '5')).toEqual(result);
+  });
+
+  it('if user not found, should throw NotFoundException', async () => {
+    await expect(controller.getUserInfo('notfound', '0', '5')).rejects.toThrow(
+      'User not found',
+    );
+  });
+
+  it('test 200 /api/users/:userName', async () => {
+    return request(app.getHttpServer())
+      .get('/api/users/test')
+      .expect(200)
+      .expect({
+        ...service.getUserByName('test'),
+        submissions: [],
+        pageEnd: 0,
+      });
+  });
+
+  it('test 404 /api/users/:userName', async () => {
+    return request(app.getHttpServer())
+      .get('/api/users/notfound')
+      .expect(404)
+      .expect({
+        statusCode: 404,
+        message: 'User not found',
+        error: 'Not Found',
+      });
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
