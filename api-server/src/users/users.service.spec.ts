7d3e786ff67df1d8e12d99b66c8826e79ced2f8e
h9661 <guscksdn123@gmail.com>
2023-12-23 20:38:55 +0900

    Refactor UsersService and fix createUser method

--- api-server/src/users/users.service.spec.ts
+++ api-server/src/users/users.service.spec.ts
@@ -1,10 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
-import { getRepositoryToken } from '@nestjs/typeorm';
 import { UserTable } from './entities/user.entity';
-import { UsersMockRepository } from './mocks/repository.mock';
-import { CreateUserDto } from './dto/create-user.dto';
-import { BadRequestException } from '@nestjs/common';
+import { UsersRepository } from './__mocks__/users.repository';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('UsersService', () => {
   let service: UsersService;
@@ -15,7 +13,7 @@ describe('UsersService', () => {
         UsersService,
         {
           provide: getRepositoryToken(UserTable),
-          useClass: UsersMockRepository,
+          useClass: UsersRepository,
         },
       ],
     }).compile();
@@ -26,77 +24,108 @@ describe('UsersService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
-});
 
-describe('UsersService', () => {
-  let service: UsersService;
-  let mockRepository: UsersMockRepository;
+  describe('createUser', () => {
+    it('should create a user', async () => {
+      const user = await service.create({
+        name: 'chanwoo',
+        email: 'chanwoo@chanwoo.com',
+        password: 'chanwoo',
+      });
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        UsersService,
-        {
-          provide: getRepositoryToken(UserTable),
-          useClass: UsersMockRepository,
-        },
-      ],
-    }).compile();
+      expect(user).toHaveProperty('id');
+      expect(user).toHaveProperty('createdAt');
+      expect(user).toHaveProperty('updatedAt');
 
-    service = module.get<UsersService>(UsersService);
-    mockRepository = module.get<UsersMockRepository>(
-      getRepositoryToken(UserTable),
-    );
+      expect(user.name).toBe('chanwoo');
+      expect(user.email).toBe('chanwoo@chanwoo.com');
+      expect(user.password).toBe('chanwoo');
+    });
+
+    it('should throw an error if user already exists', async () => {
+      await expect(
+        service.create({
+          name: 'test',
+          email: 'abcd@abcd.com',
+          password: 'abcd',
+        }),
+      ).rejects.toThrow('이미 존재하는 이름입니다.');
+    });
+
+    it('should throw an error if email already exists', async () => {
+      await expect(
+        service.create({
+          name: 'abcd',
+          email: 'test@test.test',
+          password: 'abcd',
+        }),
+      ).rejects.toThrow('이미 존재하는 이메일입니다.');
+    });
+
+    afterAll(() => {
+      jest.clearAllMocks();
+    });
   });
 
-  describe('create', () => {
-    it('should create a user', async () => {
-      const createUserDto: CreateUserDto = {
-        name: 'John Doe',
-        email: 'johndoe@example.com',
-        password: 'password',
-      };
-      jest.spyOn(service, 'create').mockImplementation(async () => {
-        return await mockRepository.create(createUserDto);
+  describe('getUserByEmail', () => {
+    it('should return a user', async () => {
+      const user = await service.getUserByEmail('test@test.test');
+
+      expect(user).toEqual({
+        name: 'test',
+        email: 'test@test.test',
+        acceptCount: 0,
+        failCount: 0,
+        winCount: 0,
+        totalCount: 0,
       });
-      const user = await service.create(createUserDto);
-      expect(user).toBeDefined();
-      expect(user.name).toEqual(createUserDto.name);
-      expect(user.email).toEqual(createUserDto.email);
-      expect(user.password).toEqual(createUserDto.password);
-      expect(user.id).toEqual(1);
-      await expect(service.create).toHaveBeenCalledWith(createUserDto);
     });
+  });
 
-    it('should throw a BadRequestException if the name already exists', async () => {
-      const createUserDto: CreateUserDto = {
-        name: 'John Doe',
-        email: 'johndoe@example.com',
-        password: 'password',
-      };
+  describe('getUserByName', () => {
+    it('should return a user', async () => {
+      const user = await service.getUserByName('test');
 
-      jest
-        .spyOn(service, 'create')
-        .mockRejectedValue(new BadRequestException());
-
-      await expect(service.create(createUserDto)).rejects.toThrow(
-        BadRequestException,
-      );
+      expect(user).toEqual({
+        name: 'test',
+        email: 'test@test.test',
+        acceptCount: 0,
+        failCount: 0,
+        winCount: 0,
+        totalCount: 0,
+      });
     });
+  });
 
-    it('should throw a BadRequestException if the email already exists', async () => {
-      const createUserDto: CreateUserDto = {
-        name: 'John Doe',
-        email: 'johndoe@example.com',
-        password: 'password',
-      };
-      jest
-        .spyOn(service, 'create')
-        .mockRejectedValue(new BadRequestException());
+  describe('increaseAcceptCount', () => {
+    it('should increase acceptCount', async () => {
+      const user = await service.increaseAcceptCount('test');
 
-      await expect(service.create(createUserDto)).rejects.toThrow(
-        BadRequestException,
-      );
+      expect(user).toHaveProperty('acceptCount', 1);
+    });
+  });
+
+  describe('increaseFailCount', () => {
+    it('should increase failCount', async () => {
+      const user = await service.increaseFailCount('test');
+
+      expect(user).toHaveProperty('failCount', 1);
+    });
+  });
+
+  describe('increaseWinCount', () => {
+    it('should increase winCount', async () => {
+      const user = await service.increaseWinCount('test');
+
+      expect(user).toHaveProperty('winCount', 1);
+    });
+  });
+
+  describe('increaseTotalCount', () => {
+    it('should increase totalCount', async () => {
+      const user = await service.increaseTotalCount('test');
+
+      expect(user).toHaveProperty('totalCount', 1);
     });
   });
 });
